52eef778deb5f9b353643c33d8c4e777e2696a02
Martin Weigert <martin.weigert@epfl.ch>
2021-11-17 23:36:35 +0100

    Fix random state issue, add tests, add torch dataloader (WIP)

--- tests/test_torch_data.py
+++ tests/test_torch_data.py
@@ -4,8 +4,7 @@ from augmend.utils import create_test_pattern
 import torch 
 from torch.utils.data import TensorDataset
 
-if __name__ == '__main__':
-
+def test_torch_data():
     y = create_test_pattern(n_samples=16, shape=(512,512), grid_w=(3,10)).astype(np.int16)
     x = (y + 50*np.random.normal(0,1,y.shape)).astype(np.float32)
 
@@ -21,8 +20,14 @@ if __name__ == '__main__':
              Elastic(axis=(0, 1), amount=5, order=0)])
     
     data = aug.torch_wrap(data)
+    return data
 
 
+if __name__ == '__main__':
+
+
+    data = test_torch_data()
+    
     x2, y2 = data[0]
 
     import matplotlib.pyplot as plt
